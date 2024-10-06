6f49f1956cf19c7051ffa15f1eef175b253782d0
rishav-3010 <vajpayeerishav@gmail.com>
2024-10-07 01:37:31 +0530

    Update build with correct paths

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'; // Use HashRouter
 import Home from './components/Home';
 import Shop from './components/Shop';
 import Cart from './components/Cart';
@@ -8,7 +8,7 @@ import PaymentSuccess from './components/PaymentSuccess';
 
 function App() {
   return (
-    <Router>
+    <Router> {/* HashRouter for GitHub Pages compatibility */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter as Router } from 'react-router-dom';  // Switch to HashRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router> {/* Wrap App with Router */}
+      <App />
+    </Router>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
